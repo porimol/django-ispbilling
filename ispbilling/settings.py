19a59645ab13d978f8b3d000c9df8b335cef8456
porimol <p.c_roy@yahoo.com>
2016-09-13 17:41:03 +0600

    internetbundles and clients app add

--- ispbilling/settings.py
+++ ispbilling/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'internetbundles'
+    'internetbundles',
+    'clients',
 ]
 
 MIDDLEWARE_CLASSES = [
